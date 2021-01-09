95512d7a2d6913484edd2180ff092e2344085cde
Highom <highomlp@gmail.com>
2021-01-09 21:17:04 +0100

    allowed CORS

--- src/main/java/ch/bbw/yr/controller/ApiController.java
+++ src/main/java/ch/bbw/yr/controller/ApiController.java
@@ -6,20 +6,19 @@ package ch.bbw.yr.controller;
 
 import ch.bbw.yr.model.PlayerData;
 import ch.bbw.yr.model.timers.TimerCollection;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class ApiController {
 
+    @CrossOrigin
     @GetMapping("/player")
     public PlayerData getAllPlayerData(@RequestParam(name= "username", required = true) String username){
         return new PlayerData(username);
     }
 
+    @CrossOrigin
     @GetMapping("/timers")
     public TimerCollection getAllTimers(){
         return new TimerCollection();
