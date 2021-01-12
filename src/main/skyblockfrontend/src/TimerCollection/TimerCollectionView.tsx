7477a1b69e1e08ed28c2ad308c98a516895b7be1
Highom <highomlp@gmail.com>
2021-01-12 22:19:47 +0100

    Frontend: changed the timers to be better readable

--- src/main/skyblockfrontend/src/TimerCollection/TimerCollectionView.tsx
+++ src/main/skyblockfrontend/src/TimerCollection/TimerCollectionView.tsx
@@ -20,9 +20,9 @@ const PlayerDataView: React.FC<TimerCollectionViewProps> = ({ timerCollection, i
             <Typography variant="h5">Timers</Typography>
             <Typography>Bank Interest is {timerCollection.bankInterest.estimateRelative}</Typography>
             <Typography>Dark Auction is {timerCollection.darkAuction.estimateRelative}</Typography>
-            <Typography>New Year event is {timerCollection.newYear.estimateRelative} for {formatDuration(intervalToDuration({start: timerCollection.newYear.estimate, end: timerCollection.newYear.endEstimate}))}</Typography>
-            <Typography>Spooky festival is {timerCollection.spookyFestival.estimateRelative}for {formatDuration(intervalToDuration({start: timerCollection.spookyFestival.estimate, end: timerCollection.spookyFestival.endEstimate}))}</Typography>
-            <Typography>Winter Event is {timerCollection.winterEvent.estimateRelative} for {formatDuration(intervalToDuration({start: timerCollection.winterEvent.estimate, end: timerCollection.winterEvent.endEstimate}))}</Typography>
+            <Typography>New Year event is {timerCollection.newYear.estimateRelative} and lasts for {formatDuration(intervalToDuration({start: timerCollection.newYear.estimate, end: timerCollection.newYear.endEstimate}))}</Typography>
+            <Typography>Spooky festival is {timerCollection.spookyFestival.estimateRelative} and lasts for {formatDuration(intervalToDuration({start: timerCollection.spookyFestival.estimate, end: timerCollection.spookyFestival.endEstimate}))}</Typography>
+            <Typography>Winter Event is {timerCollection.winterEvent.estimateRelative} and lasts for {formatDuration(intervalToDuration({start: timerCollection.winterEvent.estimate, end: timerCollection.winterEvent.endEstimate}))}</Typography>
         </Box>
     );
 };
